b870242c935b59ab0f042f1b4567f374c671fe2d
nhuthq <nhuthq@zigexn.vn>
2025-04-04 16:27:53 +0700

    finish nav

--- src/components/views/Home/index.jsx
+++ src/components/views/Home/index.jsx
@@ -4,15 +4,12 @@ import Footer from "../../Footer";
 import Hero from "@/components/Hero";
 import Articles from "@/components/Articles";
 import CTA from "@/components/CTA";
+import NavBar from "@/components/NavBar";
 
 function Home() {
   return (
     <div>
-      <Header />
-      <Hero />
-      <Articles />
-      <CTA />
-      {/* <Footer /> */}
+      <NavBar />
     </div>
   );
 }
