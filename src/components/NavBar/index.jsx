8bfff9db17e04bc546722089eb1c4e2faae3a779
nhuthq <nhuthq@zigexn.vn>
2025-04-06 16:15:02 +0700

    finish

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -7,27 +7,29 @@ function NavBar() {
   const [isShowSearchBox, setIsShowSearchBox] = useState(false);
 
   return (
-    <nav className="stick w-full h-20 z-10 px-10 md:px-20 py-5 top-0 flex items-center justify-between gap-12 border-b border-stone-200 bg-white">
-      <Link to={"/"}>
-        <img
-          src="vite.svg"
-          className="w-10 flex-none cursor-pointer"
-          alt="logo"
-        />
-      </Link>
-      <div
-        className={`absolute md:relative w-full left-0 top-20 md:top-0 px-10 md:px-0 py-6 border-b border-stone-200 md:border-none transition-all ${
-          isShowSearchBox
-            ? "opacity-100 pointer-events-auto"
-            : "opacity-0 pointer-events-none duration-100"
-        } md:opacity-100 md:pointer-events-auto`}
-      >
-        <input
-          type="text"
-          placeholder="Search..."
-          className="w-full md:w-1/4 p-4 md:pl-12 bg-stone-200 rounded-full placeholder:text-stone-500 placeholder:text-sm md:placeholder:text-md focus:bg-transparent"
-        />
-        <IoSearch className="absolute right-14 md:pointer-events-none md:left-5 top-1/2 text-xl text-stone-500 -translate-y-1/2" />
+    <nav className="stick w-full h-20 px-10 lg:px-20 py-5 top-0 flex items-center justify-between border-b border-stone-200 bg-white gap-2 md:gap-5">
+      <div className="w-full h-full flex flex-row items-center justify-start gap-2 md:gap-5">
+        <Link to={"/"}>
+          <img
+            src="vite.svg"
+            className="w-10 flex-none cursor-pointer"
+            alt="logo"
+          />
+        </Link>
+        <div
+          className={`absolute md:relative w-full md:w-1/2 h-full left-0 top-20 md:top-0 px-10 md:px-0 border-b border-stone-200 md:border-none transition-all ${
+            isShowSearchBox
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none duration-100"
+          } md:opacity-100 md:pointer-events-auto`}
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full h-full md:pl-12 bg-stone-200 rounded-full placeholder:text-stone-500 placeholder:text-sm md:placeholder:text-md focus:bg-transparent"
+          />
+          <IoSearch className="absolute right-14 md:pointer-events-none md:left-5 top-1/2 text-xl text-stone-500 -translate-y-1/2" />
+        </div>
       </div>
       <div className="flex flex-row items-center gap-2 md:gap-5">
         <div
